415652b10b23ede33ddf280a92f60ff2eca0e43e
koush <koush@example.com>
2026-08-01 17:24:34 -0700

    web_helpers: import By to fix NameError in fallback input logic

--- ninjemail/utils/web_helpers.py
+++ ninjemail/utils/web_helpers.py
@@ -2,6 +2,7 @@
 from typing import Tuple
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support import expected_conditions as EC
